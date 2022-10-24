d84d541c850af4f8d160a6c37907a7c049e3614e
Ann <niyta@mail.ru>
2022-10-24 15:35:04 +0500

    ДЗ доработано))

--- src/Main.java
+++ src/Main.java
@@ -34,67 +34,51 @@ public class Main {
            counterG++;
        }
         System.out.println();
-        Main.addNumbers("str1", 2);
+        Main.addNumbers("str1", 1);
         Main.addNumbers("str2", 1);
-        Main.addNumbers("str1", 5);
-       // Main.addNumbers("str1", 5);   если раскоментировать выбрасывается исключение
+        Main.addNumbers("str3", 2);
+        Main.addNumbers("str1", 2);
+        Main.addNumbers("str1", 1);
+       // Main.addNumbers("str2", 1);// если раскоментировать выбрасывается исключение
         System.out.println(numbers);
-
-
         System.out.println();
-        List<Integer> number1 = Arrays.asList(5, 6, 8); //19
-        List<Integer> number2 = Arrays.asList(521, 658, 842); //2021
-        List<Integer> number3 = Arrays.asList(1, 326, 88); //415
-        List<Integer> number4 = Arrays.asList(77, 26, 118); // 221
-        List<Integer> number5 = Arrays.asList(115, 3, 58); //176
-        String string1 = "string1";
-        String string2 = "string2";
-        String string3 = "string3";
-        String string4 = "string4";
-        String string5 = "string5";
 
         Map<String, List<Integer>> random = new HashMap<>();
-        random.put(string1, number1);
-        random.put(string2, number2);
-        random.put(string3, number3);
-        random.put(string4, number4);
-        random.put(string5, number5);
-        //System.out.println(random);
+        random.put("первая", Arrays.asList(5, 6, 8)); //19
+        random.put("вторая", Arrays.asList(521, 658, 842)); //2021
+        random.put("третья", Arrays.asList(1, 326, 88)); //415
+        random.put("четвертая", Arrays.asList(77, 26, 118)); //221
+        random.put("пятая", Arrays.asList(115, 3, 58)); //176
+
         random.forEach((key, value) ->System.out.println(key + " " + value));
-        Integer one = number1.get(0) + number1.get(1) + number1.get(2);
-        Integer two = number2.get(0) + number2.get(1) + number2.get(2);
-        Integer three = number3.get(0) + number3.get(1) + number3.get(2);
-        Integer four = number4.get(0) + number4.get(1) + number4.get(2);
-        Integer five = number5.get(0) + number5.get(1) + number5.get(2);
         System.out.println();
 
         Map<String, Integer> remake = new HashMap<>();
-        remake.put(string1, one);
-        remake.put(string2, two);
-        remake.put(string3, three);
-        remake.put(string4, four);
-        remake.put(string5, five);
-        //System.out.println(remake);
+        random.forEach((key, value) -> remake.put(key, value.get(0) + value.get(1) + value.get(2)));
+
         remake.forEach((key, value) ->System.out.println(key + " " + value));
         System.out.println();
 
-        Map<Integer, String> random2 =new TreeMap<>();
+        Map<Integer, String> random2 = new LinkedHashMap<>();
+        random2.put(7,"седьмой");
+        random2.put(10,"десятый");
         random2.put(1,"первый");
+        random2.put(4,"четвертый");
         random2.put(2,"второй");
         random2.put(3,"третий");
-        random2.put(4,"четвертый");
         random2.put(5,"пятый");
         random2.put(6,"шестой");
-        random2.put(7,"седьмой");
         random2.put(8,"восьмой");
         random2.put(9,"девятый");
-        random2.put(10,"десятый");
+
         System.out.println(random2);
 
     }
     public static Map<String, Integer> numbers = new HashMap<>();
+    public static Map<String, Integer> numbers2 = new HashMap<>();
     public static void addNumbers(String string, Integer integer){
-       if(numbers.containsKey(string) && numbers.containsValue(integer)){
+        numbers2.put(string, integer);
+       if(numbers.equals(numbers2)){
        throw new IllegalArgumentException("дважды добавить одно и то же нельзя!!");
         }else {
            numbers.put(string, integer);
